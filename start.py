940686c26fbd0cf8a6f9e8254ed623d5f345882a
Yan Jiang <j582033@163.com>
2016-08-04 13:36:03 +0800

    整理文件

--- start.py
+++ start.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf8 -*-
-import time
+import time, sys
 from analysis import isolated
+from Queue import Queue
+from threading import Thread
 
 
 if __name__ == '__main__':
     url = 'http://www.itjuzi.com/company/'
-    for i in range(74, 500):
+    start = int(sys.argv[1])
+    end = int(sys.argv[2])+1
+    for i in range(start, end):
         isolated(url, i)
-        time.sleep(3)
+        time.sleep(1)
+    sys.exit()
