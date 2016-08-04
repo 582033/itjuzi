869d525cd44c97fcc7c3ac7846250ddcabdefeff
Yan Jiang <j582033@163.com>
2016-08-04 14:42:40 +0800

    多线程补遗

--- supplement.py
+++ supplement.py
@@ -3,8 +3,9 @@
 import requests, json, re, sys, MySQLdb, cookielib, time
 from analysis import isolated
 from threading import Thread
+from Queue import Queue
 
-def get_data(start, limit):
+def get_data(start, limit, queue):
     conn = MySQLdb.connect(host='localhost', user='root', passwd='', db='itjuzi', charset='utf8')
     cursor = conn.cursor()
     sql = "select juzi_id from faild limit %s,%s" % (start, limit)
@@ -14,7 +15,13 @@ def get_data(start, limit):
         for juzi_id in i:
             faild_list.append(juzi_id)
     conn.close()
-    return faild_list
+    queue.put(faild_list)
+
+def loop(flist):
+    url = 'http://www.itjuzi.com/company/'
+    for i in flist:
+        isolated(url, i)
+        time.sleep(1)
 
 #多线程补遗
 if __name__ == '__main__':
@@ -22,12 +29,11 @@ if __name__ == '__main__':
     start = 0
     end = 3000
     workload = (end - start) / thread_count
+    queue = Queue()
     for i in range(1, thread_count):
         t_start = start + workload * i
         t_end = start + workload * (i+1) - 1
-        flist = get_data(t_start, t_end)
+        Thread(target=get_data, args=(t_start, t_end, queue)).start()
+        Thread(target=loop, args=(queue.get(), )).start()
 
-        url = 'http://www.itjuzi.com/company/'
-        for id in flist:
-            isolated(url, id)
-            time.sleep(1)
+    sys.exit()
